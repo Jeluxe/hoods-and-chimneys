dab32d1c4a69dda3513a60c4fc4615661dcd2769
Jeluxe <metalshay54@gmail.com>
2024-06-17 15:49:52 +0300

    Built About page with dummy data

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,44 @@
+import "./About.css"
+
 const About = () => {
   return (
-    <div>About</div>
+    <div className="about">
+      <div className="wrapper">
+        <section>
+          <img src="" width={300} height={350} />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo velit ipsam distinctio ullam assumenda explicabo, nihil ut! Aliquam deleniti officiis quae corporis laboriosam. Et omnis nostrum ipsa? Ullam, explicabo fugiat!
+          Laudantium ratione quos fuga repellat beatae dolore ut facere amet distinctio culpa nemo, vero dolorem optio asperiores reiciendis molestias consectetur earum soluta dicta tempore! Accusamus neque nesciunt esse necessitatibus unde.
+          Magni doloremque aliquam exercitationem vero, distinctio nesciunt totam tenetur? Dolorem iusto officiis esse neque repellat modi a unde? Obcaecati nesciunt tempore voluptatum ipsam eius ipsum perferendis, quo qui autem dolor?
+          Nisi nostrum esse porro perspiciatis eius tempora minima. Consectetur architecto numquam deleniti voluptatem vitae nemo id labore voluptas repudiandae eius culpa molestiae dolores doloribus porro, delectus beatae! Ab, commodi quas.
+          Eveniet delectus laboriosam magnam in blanditiis assumenda vel deleniti quidem provident inventore molestias voluptas incidunt optio ratione quis aperiam, deserunt architecto expedita consequuntur, officiis ipsa atque. Architecto atque aut reiciendis?
+        </section>
+        <section>
+          <img src="" width={300} height={350} />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo velit ipsam distinctio ullam assumenda explicabo, nihil ut! Aliquam deleniti officiis quae corporis laboriosam. Et omnis nostrum ipsa? Ullam, explicabo fugiat!
+          Laudantium ratione quos fuga repellat beatae dolore ut facere amet distinctio culpa nemo, vero dolorem optio asperiores reiciendis molestias consectetur earum soluta dicta tempore! Accusamus neque nesciunt esse necessitatibus unde.
+          Magni doloremque aliquam exercitationem vero, distinctio nesciunt totam tenetur? Dolorem iusto officiis esse neque repellat modi a unde? Obcaecati nesciunt tempore voluptatum ipsam eius ipsum perferendis, quo qui autem dolor?
+          Nisi nostrum esse porro perspiciatis eius tempora minima. Consectetur architecto numquam deleniti voluptatem vitae nemo id labore voluptas repudiandae eius culpa molestiae dolores doloribus porro, delectus beatae! Ab, commodi quas.
+          Eveniet delectus laboriosam magnam in blanditiis assumenda vel deleniti quidem provident inventore molestias voluptas incidunt optio ratione quis aperiam, deserunt architecto expedita consequuntur, officiis ipsa atque. Architecto atque aut reiciendis?
+        </section>
+        <section>
+          <img src="" width={300} height={350} />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo velit ipsam distinctio ullam assumenda explicabo, nihil ut! Aliquam deleniti officiis quae corporis laboriosam. Et omnis nostrum ipsa? Ullam, explicabo fugiat!
+          Laudantium ratione quos fuga repellat beatae dolore ut facere amet distinctio culpa nemo, vero dolorem optio asperiores reiciendis molestias consectetur earum soluta dicta tempore! Accusamus neque nesciunt esse necessitatibus unde.
+          Magni doloremque aliquam exercitationem vero, distinctio nesciunt totam tenetur? Dolorem iusto officiis esse neque repellat modi a unde? Obcaecati nesciunt tempore voluptatum ipsam eius ipsum perferendis, quo qui autem dolor?
+          Nisi nostrum esse porro perspiciatis eius tempora minima. Consectetur architecto numquam deleniti voluptatem vitae nemo id labore voluptas repudiandae eius culpa molestiae dolores doloribus porro, delectus beatae! Ab, commodi quas.
+          Eveniet delectus laboriosam magnam in blanditiis assumenda vel deleniti quidem provident inventore molestias voluptas incidunt optio ratione quis aperiam, deserunt architecto expedita consequuntur, officiis ipsa atque. Architecto atque aut reiciendis?
+        </section>
+        <section>
+          <img src="" width={300} height={350} />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo velit ipsam distinctio ullam assumenda explicabo, nihil ut! Aliquam deleniti officiis quae corporis laboriosam. Et omnis nostrum ipsa? Ullam, explicabo fugiat!
+          Laudantium ratione quos fuga repellat beatae dolore ut facere amet distinctio culpa nemo, vero dolorem optio asperiores reiciendis molestias consectetur earum soluta dicta tempore! Accusamus neque nesciunt esse necessitatibus unde.
+          Magni doloremque aliquam exercitationem vero, distinctio nesciunt totam tenetur? Dolorem iusto officiis esse neque repellat modi a unde? Obcaecati nesciunt tempore voluptatum ipsam eius ipsum perferendis, quo qui autem dolor?
+          Nisi nostrum esse porro perspiciatis eius tempora minima. Consectetur architecto numquam deleniti voluptatem vitae nemo id labore voluptas repudiandae eius culpa molestiae dolores doloribus porro, delectus beatae! Ab, commodi quas.
+          Eveniet delectus laboriosam magnam in blanditiis assumenda vel deleniti quidem provident inventore molestias voluptas incidunt optio ratione quis aperiam, deserunt architecto expedita consequuntur, officiis ipsa atque. Architecto atque aut reiciendis?
+        </section>
+
+      </div>
+    </div>
   )
 }
 
