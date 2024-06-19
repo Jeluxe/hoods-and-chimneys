f6b38890dbbd871ebf2c900fbdf6918f27d93a37
Jeluxe <metalshay54@gmail.com>
2024-06-20 01:53:27 +0300

    Created a simplified Contact page.

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,30 @@
+import { useState } from "react"
+import "./Contact.css"
+
 const Contact = () => {
+  const [email, setEmail] = useState<string>("")
+  const [name, setName] = useState<string>("")
+  const [notes, setNotes] = useState<string>("")
+
+  const onClick = () => {
+    if ([email, name].every(field => field?.trim() !== "")) {
+      alert(`בקשה נשלחה בהצלחה`)
+      console.log({ email, name, notes })
+    }
+  }
+
   return (
-    <div>Contact</div>
+    <div className="contact">
+      <h1>צרו קשר</h1>
+      <br />
+      <div className="contact-inputs">
+        <div><span>אימייל:</span><input onChange={({ target: { value } }) => setEmail(value)} /></div>
+        <div><span>שם מלא:</span><input onChange={({ target: { value } }) => setName(value)} /></div>
+        <div><span>הערות:</span><textarea onChange={({ target: { value } }) => setNotes(value)} /></div>
+      </div>
+      <br />
+      <button onClick={onClick}>שליחה</button>
+    </div>
   )
 }
 
