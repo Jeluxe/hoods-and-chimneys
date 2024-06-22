60f2dad627b1679a3ae1a2b04e05eac276dbc103
Jeluxe <metalshay54@gmail.com>
2024-06-23 00:16:58 +0300

    Added fields

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,13 +3,14 @@ import "./Contact.css"
 
 const Contact = () => {
   const [email, setEmail] = useState<string>("")
-  const [name, setName] = useState<string>("")
-  const [notes, setNotes] = useState<string>("")
+  const [phone, setPhone] = useState<string>("")
+  const [subject, setSubject] = useState<string>("")
+  const [message, setMessage] = useState<string>("")
 
   const onClick = () => {
-    if ([email, name].every(field => field?.trim() !== "")) {
+    if ([email, phone, subject, message].every(field => field?.trim() !== "")) {
       alert(`בקשה נשלחה בהצלחה`)
-      console.log({ email, name, notes })
+      console.log({ email, phone, subject, message })
     }
   }
 
@@ -18,9 +19,10 @@ const Contact = () => {
       <h1>צרו קשר</h1>
       <br />
       <div className="contact-inputs">
-        <div><span>אימייל:</span><input onChange={({ target: { value } }) => setEmail(value)} /></div>
-        <div><span>שם מלא:</span><input onChange={({ target: { value } }) => setName(value)} /></div>
-        <div><span>הערות:</span><textarea onChange={({ target: { value } }) => setNotes(value)} /></div>
+        <div className="field-email"><span>אימייל:</span><input onChange={({ target: { value } }) => setEmail(value)} /></div>
+        <div className="field-phone"><span>טלפון:</span><input onChange={({ target: { value } }) => setPhone(value)} /></div>
+        <div className="field-subject"><span>נושא:</span><input onChange={({ target: { value } }) => setSubject(value)} /></div>
+        <div className="field-message"><span>הערות:</span><textarea onChange={({ target: { value } }) => setMessage(value)} rows={7} maxLength={580} /></div>
       </div>
       <br />
       <button onClick={onClick}>שליחה</button>
