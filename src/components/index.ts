9ddc840969bb4b007ad4f13f42c4c86db88ccc3f
Jeluxe <metalshay54@gmail.com>
2024-07-18 00:52:25 +0300

    fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,13 +3,11 @@ import Footer from "./Footer/Footer"
 import Card from "./Card/Card"
 import Cards from "./Cards/Cards"
 import Dropdown from "./Dropdown/Dropdown"
-import FilterMenu from "./FilterMenu/FilterMenu"
 import Carousel from "./Carousel/Carousel"
 
 export {
   Navbar,
   Footer,
-  FilterMenu,
   Dropdown,
   Card,
   Cards,
--- src/config/router.tsx
+++ src/config/router.tsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter(
       <Route path=":category" element={<Products />} />
       <Route path=":category/:productId" element={<Product />} />
       <Route path="contact" element={<Contact />} />
-      <Route path="*" element={<div>Page not found 404</div>} />
+      <Route path="404" element={<div>Page not found 404</div>} />
     </Route >
   )
 );
