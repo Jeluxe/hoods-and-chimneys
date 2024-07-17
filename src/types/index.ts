b7e41c83f16eba6fa5002ad9e2d8d83c4cbb0f3f
Jeluxe <metalshay54@gmail.com>
2024-07-17 23:56:59 +0300

    chore: Reoraganized types

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,19 +3,3 @@ declare module "react" {
     '--t'?: string
   }
 }
-
-export interface ProductProps {
-  id: string,
-  name: string,
-  description: string,
-  price: number,
-  categories: string[]
-  image: string
-}
-
-export type FilterOptions = {
-  type: { id: string, name: string, checked: boolean }[],
-  price: { min: number, max: number }
-}
-
-export type displayedProductProps = Omit<ProductProps, "description">
---
+++ src/types/product.ts
@@ -0,0 +1,15 @@
+export interface ProductProps {
+  id: string,
+  name: string,
+  description: string,
+  price: number,
+  categories: string[]
+  image: string
+}
+
+export type FilterOptions = {
+  type: { id: string, name: string, checked: boolean }[],
+  price: { min: number, max: number }
+}
+
+export type displayedProductProps = Omit<ProductProps, "description">
